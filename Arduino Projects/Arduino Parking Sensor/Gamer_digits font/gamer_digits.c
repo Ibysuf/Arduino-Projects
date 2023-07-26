f26e531c6a19b74649f2fe7818010586fb817523
Ibysuf <102690063+Ibysuf@users.noreply.github.com>
2023-07-26 12:44:14 +0100

    Add files via upload

---
+++ Arduino Projects/Arduino Parking Sensor/Gamer_digits font/gamer_digits.c
@@ -0,0 +1,22 @@
+/*
+  Fontname: gamer_digits
+  Copyright: Created with Fony 1.4.7
+  Capital A Height: 0, '1' Height: 6
+  Calculated Max Values w= 5 h= 6 x= 1 y= 0 dx= 6 dy= 0 ascent= 0 len= 6
+  Font Bounding box     w= 9 h= 5 x= 0 y=-6
+  Calculated Min Values           x= 0 y=-6 dx= 0 dy= 0
+  Pure Font   ascent = 6 descent= 0
+  X Font      ascent = 6 descent= 0
+  Max Font    ascent = 0 descent=-6
+*/
+#include "u8g.h"
+const u8g_fntpgm_uint8_t gamer_digits[137] U8G_FONT_SECTION("gamer_digits") = {
+  0,9,5,0,250,6,0,0,0,0,48,57,0,0,250,6,
+  0,5,6,6,6,0,250,248,216,216,216,216,248,4,6,6,
+  6,1,250,224,96,96,96,96,240,5,6,6,6,0,250,248,
+  24,120,192,192,248,5,6,6,6,0,250,248,24,248,24,216,
+  248,5,6,6,6,0,250,56,120,216,248,24,24,5,6,6,
+  6,0,250,248,192,248,24,24,248,5,6,6,6,0,250,248,
+  192,248,216,216,248,5,6,6,6,0,250,248,24,24,24,24,
+  24,5,6,6,6,0,250,248,216,248,216,216,248,5,6,6,
+  6,0,250,248,216,216,248,24,248};
